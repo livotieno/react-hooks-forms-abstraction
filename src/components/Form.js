2fc97632b63ef2f39d9f59b7aca3c88d502879c4
livotieno <livio5261@gmail.com>
2023-11-02 10:15:34 +0300

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,66 @@
 import React, { useState } from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+const Form = () => {
+  // initialize state/state of entire application :)
+  const [state, setState] = useState({
+    firstname: "",
+    lastname: "",
+    admin: false,
+  });
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
+  // !DRY/instead of having eventHandler for each input type
+  const handleChange = (e) => {
+    const name = e.target.name;
+    let value = e.target.value;
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
-  }
+    // case of checkbox
+    if (e.target.type === "checkbox") {
+      value = e.target.checked;
+    }
+
+    // !spread oldState & return newState/update old state
+    // !using dynamic object properties
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(state);
+  };
 
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="firstname"
+        id="firstname"
+        value={state.firstname}
+        onChange={handleChange}
+      />
+
+      <input
+        type="text"
+        name="lastname"
+        id="lastname"
+        value={state.lastname}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="admin">Admin</label>
+      <input
+        type="checkbox"
+        name="admin"
+        id="admin"
+        checked={state.admin}
+        onChange={handleChange}
+      />
+
+      <input type="submit" />
     </form>
   );
-}
+};
 
 export default Form;
